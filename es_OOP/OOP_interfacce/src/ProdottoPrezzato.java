2ffeacd520882855cf7eea5ebe6f6ad7da68ecb2
Alice <43543176+alisola21@users.noreply.github.com>
2021-04-15 14:52:26 +0200

    Delete es_OOP/OOP_interfacce directory

--- es_OOP/OOP_interfacce/src/ProdottoPrezzato.java
+++
@@ -1,7 +0,0 @@
-
-public interface ProdottoPrezzato {
-	public String getDescrizione(); //restituisce un'estensione del prodotto
-	
-	public double getPrezzo(); //restituisce il prezzo dell'articolo
-	
-}
